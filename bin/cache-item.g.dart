e48c3360a1231d825d09f7083ae8400573f07fb9
Ben <deckard1@gmail.com>
2020-08-18 10:00:32 -0700

    example complete, now with generated file

---
+++ bin/cache-item.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'cache-item.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CacheItem _$CacheItemFromJson(Map<String, dynamic> json) {
+  return CacheItem(
+    json['created'] as int,
+    json['keywords'] as String,
+    json['response'] as String,
+  );
+}
+
+Map<String, dynamic> _$CacheItemToJson(CacheItem instance) => <String, dynamic>{
+      'created': instance.created,
+      'keywords': instance.keywords,
+      'response': instance.response,
+    };
