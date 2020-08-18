ba28db83f3d645ea4af1d09b3b85c2954ade09df
Ben <deckard1@gmail.com>
2020-08-18 09:59:53 -0700

    example complete

--- bin/cache-item.dart
+++ bin/cache-item.dart
@@ -1,7 +1,15 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'cache-item.g.dart';
+
+@JsonSerializable()
 class CacheItem {
   int created;
   String keywords;
   String response;
 
   CacheItem(this.created, this.keywords, this.response);
+
+  factory CacheItem.fromJson(Map<String,dynamic> json) => _$CacheItemFromJson(json);
+  Map<String,dynamic> toJson() => _$CacheItemToJson(this);
 }
